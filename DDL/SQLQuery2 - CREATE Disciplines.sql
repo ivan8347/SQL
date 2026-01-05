4f1ae385b4a8c1dc8dd750de7dfb0be93cf00920
kit <kit@DESKTOP-EHKDJE1>
2026-01-06 00:57:17 +0300

    Diagram

--- DDL/SQLQuery2 - CREATE Disciplines.sql
+++ DDL/SQLQuery2 - CREATE Disciplines.sql
@@ -13,6 +13,31 @@ USE SPU_411_DDL;
 --teacher		INT,       
 --discipline  SMALLINT ,
 --PRIMARY KEY (teacher,discipline),
---CONSTRAINT FK_TDR_Taecher	 FOREIGN KEY (teacher)		REFERENCES Teachers (teacher_id),
+--CONSTRAINT FK_TDR_Taecher	   FOREIGN KEY (teacher)	  REFERENCES Teachers (teacher_id),
 --CONSTRAINT FK_TDR_Discipline FOREIGN KEY(discipline)    REFERENCES Disciplines (discipline_id),
+--);
+
+--CREATE TABLE DisciplinesDirectionsRelation
+--(
+--discipline SMALLINT,
+--direction TINYINT,
+--PRIMARY KEY(discipline,direction),
+--CONSTRAINT FK_DDR_Discipline FOREIGN KEY (discipline) REFERENCES Disciplines(discipline_id),
+--CONSTRAINT FK_DDR_Direction  FOREIGN KEY (direction)  REFERENCES Directions(direction_id)
+--);
+--CREATE TABLE RequiredDisciplines
+--(
+--discipline SMALLINT,
+--required_discipline SMALLINT,
+--PRIMARY KEY (discipline,required_discipline),
+--CONSTRAINT FK_RD_Discipline	FOREIGN KEY (discipline)		REFERENCES Disciplines(discipline_id),
+--CONSTRAINT FK_RD_Required	FOREIGN KEY (required_discipline)   REFERENCES Disciplines(discipline_id)
+--);
+--CREATE TABLE DependentDisciplines
+--(
+--discipline SMALLINT,
+--dependent_discipline SMALLINT,
+--PRIMARY KEY(discipline,dependent_discipline),
+--CONSTRAINT FK_DD_Discipline FOREIGN KEY (discipline) REFERENCES Disciplines(discipline_id),
+--CONSTRAINT FK_DD_Dependent FOREIGN KEY (dependent_discipline) REFERENCES Disciplines(discipline_id)
 --);
